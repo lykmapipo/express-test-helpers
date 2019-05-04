64de7d50d340c00a7feda9fefb3af5228cfa1095
lykmapipo <lallyelias87@gmail.com>
2019-05-04 12:56:00 +0300

    feat: export common test helpers and testApp

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import {
   sinon,
   spy,
 } from '@lykmapipo/test-helpers';
+import { testApp } from '@lykmapipo/express-common';
 
-export { chai, expect, faker, mock, should, sinon, spy };
+export { chai, expect, faker, mock, should, sinon, spy, testApp };
